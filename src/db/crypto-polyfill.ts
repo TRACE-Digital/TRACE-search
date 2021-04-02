a949ebdac5739995215e2b4fe3c26509f28eff5d
Jack McKernan <jmcker@outlook.com>
2021-04-02 13:37:20 -0400

    Add a polyfill so that crypto-pouch/native-crypto/pbkdf2 loads via <script>

--- src/db/crypto-polyfill.ts
+++ src/db/crypto-polyfill.ts
@@ -1,5 +1,11 @@
 import * as Buffer from 'buffer';
 (window as any).global = window;
+(window as any).process = {};
+(window as any).process = window;
+(window as any).process.browser = true;
+(window as any).process.version = '';
+(window as any).process.versions = { node: false };
+(window as any).process.nextTick = setTimeout;
 (window as any).global.Buffer = Buffer.Buffer;
 
 declare global {
